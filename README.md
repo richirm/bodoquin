9eb3e19b17efb7a8a83608fe2469e0c5db8bd6ac
rarosales <rarosales@indracompany.com>
2021-05-25 00:42:44 -0500

    Inicio del proyecto

---
+++ README.md
@@ -0,0 +1,26 @@
+Aplicación bodoquin
+-------------------
+
+1. Estructura:
+  - bodoquin.html => Página web principal de mi aplicación.
+  - assets/ => carpeta donde se van a almacenar recursos que no son de programación
+    - img/ => carpeta donde se van a almacenar imagenes
+    - fonts/ => carpeta donde se almacena los tipos de fuentes de la aplicación
+    
+    
+    
+2. Funcionalidades:
+  - HTML5 => conjunto de etiquetas que me permiten diseñar el esqueleto o la maqueta de mi aplicación, y por defecto me brinda un estilo y una lógica
+  - CSS3 => conjunto de selectores y estilos que me van a permitir diseñar mi aplicación 
+  - Javascript => 
+      tipos de datos:
+        string => 'hola'
+        number => 1
+        boolean => true, false
+        undefined
+        null
+        objetos
+        arrays
+  - GIT: herramienta para sincronizar trabajo y código fuente      
+   
+  
---
+++ bodoquin.js
@@ -0,0 +1,65 @@
+var pedidos = [];
+
+function sumarYAlertarValores(campo1, campo2) {
+  var sumaDeValores = campo1 + campo2;
+  alert(sumaDeValores);
+  return sumaDeValores;
+}
+
+function multiplicarYAlertarValores(campo1, campo2) {
+  var productoValores = campo1 * campo2;
+  confirm(productoValores);
+  return productoValores;
+}
+                                       
+function sumarYMultiplicarYAlertarValores(campo1, campo2, campo3) {
+  var sumaDeValores = sumarYAlertarValores(campo1, campo2);
+  var productoValores = multiplicarYAlertarValores(sumaDeValores, campo3);
+  return productoValores;
+}
+
+function realizarPedido() {
+  var nombrePostre = document.querySelector('[name="nombre_postre"]').value;
+  var cantidad = document.querySelector('[name="cantidad"]').value;
+  var correo = document.querySelector('[name="correo"]').value;
+  var departamento = document.querySelector('[name="departamento"]').value;
+  var provincia = document.querySelector('[name="provincia"]').value;
+  var distrito = document.querySelector('[name="distrito"]').value;
+  var comentario = document.querySelector('[name="comentario"]').value;
+  
+  var pedido = {
+    nombrePostre: nombrePostre,
+    cantidad: cantidad,
+    correo: correo,
+    departamento: departamento,
+    provincia: provincia,
+    distrito: distrito,
+    comentario: comentario,
+  };
+  
+  console.log(pedido);  
+}
+
+function agregarPedido() {
+  var nombrePostre = document.querySelector('[name="nombre_postre"]').value;
+  var cantidad = document.querySelector('[name="cantidad"]').value;
+  var correo = document.querySelector('[name="correo"]').value;
+  var departamento = document.querySelector('[name="departamento"]').value;
+  var provincia = document.querySelector('[name="provincia"]').value;
+  var distrito = document.querySelector('[name="distrito"]').value;
+  var comentario = document.querySelector('[name="comentario"]').value;
+  
+  var pedido = {
+    nombrePostre: nombrePostre,
+    cantidad: cantidad,
+    correo: correo,
+    departamento: departamento,
+    provincia: provincia,
+    distrito: distrito,
+    comentario: comentario,
+  };
+  
+  pedidos.push(pedido);
+  
+  console.log(pedidos);
+}

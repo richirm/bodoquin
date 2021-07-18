28c796fd08faeeffb16e3a2cbc120a205a44dc49
richirm <richirm89@hotmail.com>
2021-07-17 23:33:42 -0500

    integrar popup de carrito en pantalla de reposteria

--- app/views/reposteria/reposteria.js
+++ app/views/reposteria/reposteria.js
@@ -183,3 +183,15 @@ function mostrarOcultarModalCategoria(nombreImagen) {
   }
 }
 /********************************************/
+
+/************* POPUP *********************/
+function mostrarOcultarPopup(selectorPopup) {
+  var popupDisplay = document.querySelector(selectorPopup).style.display;
+
+  if(popupDisplay === 'inline-block') {
+    document.querySelector(`${selectorPopup}`).style.display = 'none';
+  } else {
+    document.querySelector(`${selectorPopup}`).style.display = 'inline-block';
+  }
+}
+/*****************************************/

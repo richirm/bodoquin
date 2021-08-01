142b5af6db3f0004da0e755a851752f913a2b22b
richirm <richirm89@hotmail.com>
2021-07-31 21:54:11 -0500

    feat(carrito): construye de forma dinamica los productos

--- app/views/carrito/carrito.js
+++ app/views/carrito/carrito.js
@@ -79,3 +79,94 @@ function mostrarOcultarPopup(selectorPopup) {
   }
 }
 /*****************************************/
+
+/************ DETALLE PRODUCTOS **********/
+var detalleProductos = [
+  {
+    idProducto: 1001,
+    nombreImg: 'torta_chocolate_fresa.jpg', 
+    nombreCategoria: 'Torta húmeda',
+    nombreProducto: 'Torta de leche de Óreo',
+    descripcionProducto: 'Torta rica en su sabor, ha ganado el premio internacional, hecha con agua manantial',
+    precioProducto: 60.25,
+    cantidadProducto: 1,
+  },
+  {
+    idProducto: 1002,
+    nombreImg: 'galaxys10.jpg', 
+    nombreCategoria: 'Samsung',
+    nombreProducto: 'Samsung galaxy s10',
+    descripcionProducto: '6 GB RAM + 128 gb almacenamiento + 5000 mah + full hd',
+    precioProducto: 1599,
+    cantidadProducto: 1,
+  },
+  {
+    idProducto: 1003,
+    nombreImg: 'nars.jpg', 
+    nombreCategoria: 'Base',
+    nombreProducto: 'Nars',
+    descripcionProducto: 'Base de maquillaje de larga duracion con alta cobertura',
+    precioProducto: 100,
+    cantidadProducto: 1,
+  },
+  {
+    idProducto: 1004,
+    nombreImg: 'muñeco.jpg', 
+    nombreCategoria: '2-4',
+    nombreProducto: 'Juguetes',
+    descripcionProducto: 'muñeca con chupon que llora y habla y remeda',
+    precioProducto: 45.75,
+    cantidadProducto: 1,
+  },
+];
+
+function construirCarritoDetalleProductos() {
+  var htmlDetalleProductos = '';
+  
+  detalleProductos.forEach(function(producto) {
+    var htmlProducto = `
+      <tr class="carrito_detalle_producto">
+        <td width="50%">
+          <img src="../../../assets/img/${producto.nombreImg}" />
+          <div class="carrito_detalle_producto_titulo">
+            <h3>${producto.nombreCategoria}</h3>
+            <h1>${producto.nombreProducto}</h1>
+          </div>
+        </td>
+        
+        <td width="15%" class="texto-derecha">S/. ${producto.precioProducto}</td>
+        
+        <td width="15%" class="texto-centrado">
+          <div class="carrito_detalle_producto_contador">        
+            <div class="carrito_detalle_producto_contador_agregar">
+              <i class="bi bi-dash-lg"></i>
+            </div>
+          
+            <div class="carrito_detalle_producto_contador_unidades">
+              <span><b>${producto.cantidadProducto}</b> un</span>
+            </div>
+            
+            <div class="carrito_detalle_producto_contador_agregar">
+              <i class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </td>
+        
+        <td width="15%" class="texto-derecha">S/. ${producto.cantidadProducto * producto.precioProducto}</td>
+        
+        <td width="5%" class="texto-centrado">
+          <i class="bi bi-trash"></i>
+        </td>
+      </tr>
+    `;
+    
+    htmlDetalleProductos = htmlDetalleProductos + htmlProducto;
+  });
+  
+  document.querySelector('.carrito_detalle_productos table tbody').innerHTML = htmlDetalleProductos;
+}
+
+construirCarritoDetalleProductos();
+/*****************************************/
+
+

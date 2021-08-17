01bc7f2257d01f6b4f5c9ff9ea5dfca12d527d39
wolfdark87 <david.roma.878@gmail.com>
2021-08-17 00:44:33 -0500

    feat(nav): inicializa componente nav.

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'bodoquin-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  menuColapsado: boolean = true;
   
+  ngOnInit() {
+    this.colapsarExpandirMenu();
+  }
+  
+  colapsarExpandirMenu() {
+    if(this.menuColapsado === true) {
+      (document.querySelector('.nav_menu') as HTMLElement).classList.remove('colapsado');
+      (document.querySelector('.nav_backdrop') as HTMLElement).style.display = 'block';
+      this.menuColapsado = false;
+    } else {
+      (document.querySelector('.nav_menu') as HTMLElement).classList.add('colapsado');
+      (document.querySelector('.nav_backdrop') as HTMLElement).style.display = 'none';
+      this.menuColapsado = true;
+    }
+  }
 }

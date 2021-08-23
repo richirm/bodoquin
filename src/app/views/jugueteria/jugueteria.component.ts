d489df9d9ad3d18d8e64389a4257bb7003a0efa6
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.com>
2021-08-22 23:24:26 -0500

    feat: diseño de jugueteria componente

--- src/app/views/jugueteria/jugueteria.component.ts
+++ src/app/views/jugueteria/jugueteria.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bodoquin-jugueteria',
   templateUrl: './jugueteria.component.html',
-  styleUrls: ['./jugueteria.component.css']
+  styleUrls: ['./jugueteria.component.scss']
 })
 export class JugueteriaComponent {
+  
+  producto = {
+    idProducto: 1001,
+    nombreImg: 'piano.jpg', 
+    nombreCategoria: '0-2',
+    nombreProducto: 'Juguetes',
+    descripcionProducto: 'Piano para bebes de deferentes colores para un mejor aprendizaje',
+    precioProducto: 50.00,
+    cantidadProducto: 0,
+  };
   
 }

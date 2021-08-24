2065707349025a05817bad8a5af7fb93f1ac319d
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.com>
2021-08-24 00:33:16 -0500

    feat: implementación de las tarjetas en el main de la vista de jugueteria

--- src/app/views/jugueteria/jugueteria.module.ts
+++ src/app/views/jugueteria/jugueteria.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { JugueteriaComponent } from './jugueteria.component';
 import { JugueteriaRoutingModule } from './jugueteria-routing.module';
 
 @NgModule({
   imports: [
+    CommonModule,
     JugueteriaRoutingModule,
   ],
   declarations: [

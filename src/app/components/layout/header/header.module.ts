b29d921375d9bb9aac1e0a89e22d21591c60eae3
wolfdark87 <david.roma.878@gmail.com>
2021-08-26 00:16:06 -0500

    feat(headerSecundario): creacion de nuevos componentes .

--- src/app/components/layout/header/header.module.ts
+++ src/app/components/layout/header/header.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 
+import { HeaderSecundarioModule } from './header-secundario/header-secundario.module';
+
 import { HeaderComponent } from './header.component';
 
 @NgModule({
+  imports: [ 
+    HeaderSecundarioModule,
+  ],   
   declarations: [
     HeaderComponent,    
   ],

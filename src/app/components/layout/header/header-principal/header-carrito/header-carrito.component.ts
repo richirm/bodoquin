98ad41653bcdb3a3aa2ff159a6f6ed8f9f95e7b5
wolfdark87 <david.roma.878@gmail.com>
2021-09-12 22:56:36 -0500

    feat(header-carrito) maquetacion de componente 2.0.

--- src/app/components/layout/header/header-principal/header-carrito/header-carrito.component.ts
+++ src/app/components/layout/header/header-principal/header-carrito/header-carrito.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ProductoInterface } from '../../../../../core/interfaces/producto.interface';
+
 @Component({
   selector: 'bodoquin-header-carrito',
   templateUrl: './header-carrito.component.html',
@@ -8,9 +10,53 @@ import { Component } from '@angular/core';
 export class HeaderCarritoComponent {
   
   showPopup: boolean = false;
+   
+  maximoPorProducto: number = 5; 
+  
+  productos: Array<ProductoInterface> = [
+    {
+      idProducto: 1001,
+      nombreImg: 'torta_selva_negra.jpg', 
+      nombreCategoria: 'Torta húmeda',
+      nombreProducto: 'Torta de leche de Óreo1',
+      descripcionProducto: '',
+      precioProducto: 60.25,
+      cantidadProducto: 3,
+    }, 
+    {
+      idProducto: 1002,
+      nombreImg: 'torta_selva_negra.jpg',
+      nombreCategoria: 'Torta húmeda2',
+      nombreProducto: 'Torta de leche de Óreo2',
+      descripcionProducto: '',
+      precioProducto: 70.50,
+      cantidadProducto: 4,
+    }, 
+    {
+      idProducto: 1003,
+      nombreImg: 'torta_selva_negra.jpg',
+      nombreCategoria: 'Torta húmeda3',
+      nombreProducto: 'Torta de leche de Óreo3',
+      descripcionProducto: '',
+      precioProducto: 45.50,
+      cantidadProducto: 2,
+    }
+  ];
   
   mostrarPopupCarrito() {
-    this.showPopup = true;
+    this.showPopup = true;   
+  }
+  
+  deleteProduct(idProducto: number) {
+    
+  }
+  
+  removeItemInProduct(idProducto: number) {
+    
+  }
+  
+  addItemInProduct(idProducto: number) {
+    
   }
   
 }

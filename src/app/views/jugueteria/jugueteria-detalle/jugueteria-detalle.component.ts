0cff3f8680bd7384c79d2fc8434b5c8b56214b43
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.com>
2021-10-07 00:24:36 -0500

    feat: (jugueteria-detalle) creacion de vista de detalle

---
+++ src/app/views/jugueteria/jugueteria-detalle/jugueteria-detalle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bodoquin-jugueteria-detalle',
+  templateUrl: './jugueteria-detalle.component.html',
+  styleUrls: ['./jugueteria-detalle.component.scss']
+})
+
+export class JugueteriaDetalleComponent {
+  
+}

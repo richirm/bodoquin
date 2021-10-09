d9f3089b8046f00cbe1db4bbe4f96153e97f95bc
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.com>
2021-10-09 00:04:47 -0500

    feat: (jugueteria-listado) implementación de tarjeta

--- src/app/views/jugueteria/jugueteria-routing.module.ts
+++ src/app/views/jugueteria/jugueteria-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
         component: JugueteriaListadoComponent
       }, 
       {
-        path: 'detalle',
+        path: 'detalle/:idProducto',
         component: JugueteriaDetalleComponent
       }
     ]

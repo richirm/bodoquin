c9ca4eb6a3343fa6a1775c5be5eeecae000d3ad9
richirm <richirm89@hotmail.com>
2021-10-09 23:17:21 -0500

    feat(reposteria-detalle.component): obtener información del producto

--- src/app/views/reposteria/reposteria-detalle/reposteria-detalle.component.ts
+++ src/app/views/reposteria/reposteria-detalle/reposteria-detalle.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ReposteriaService } from '../reposteria.service';
+import { ProductoInterface } from '../../../core/interfaces/producto.interface';
 
 @Component({
   selector: 'bodoquin-reposteria-detalle',
   templateUrl: './reposteria-detalle.component.html',
   styleUrls: ['./reposteria-detalle.component.scss']
 })
-export class ReposteriaDetalleComponent {
+export class ReposteriaDetalleComponent implements OnInit {
+  
+  producto: ProductoInterface;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private reposteriaService: ReposteriaService) {}
+    
+  ngOnInit() {
+    this.getInfoProducto();
+  }
+  
+  getInfoProducto() {
+    // Capturar el id de la url
+    const idProducto = this.route.snapshot.params.idProducto;
+    console.log(idProducto);
+    
+    // Obtener la información del producto de acuerdo a su id
+    this.producto = this.reposteriaService.obtenerProducto(idProducto);
+    console.log(this.producto);
+    
+    // mostrar el producto en el html
+    
+    // colocar el diseño de la vista de detalle
+    
+    // integrarnos al componente de carrito
+    
+    // componente de tabs
+  }
   
 }

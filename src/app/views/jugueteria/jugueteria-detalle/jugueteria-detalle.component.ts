08b92f1dfc1fa0c973f7beeaddcf57d62279d587
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.com>
2021-10-21 00:16:21 -0500

    feat(jugueteria-detalle) implementación de mouseLeave

--- src/app/views/jugueteria/jugueteria-detalle/jugueteria-detalle.component.ts
+++ src/app/views/jugueteria/jugueteria-detalle/jugueteria-detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { JugueteriaService } from '../jugueteria.service';
@@ -11,6 +11,9 @@ import { CarritoService } from '../../../core/services/carrito.service';
   styleUrls: ['./jugueteria-detalle.component.scss']
 })
 export class JugueteriaDetalleComponent implements OnInit {
+  
+  @ViewChild('elementoContenedorZoom', {static: false}) elementoContenedorZoom;
+  @ViewChild('elementoImagenZoom', {static: false}) elementoImagenZoom;
 
   producto: ProductoInterface;
 
@@ -49,4 +52,22 @@ export class JugueteriaDetalleComponent implements OnInit {
     this.carritoService.productoRestado.emit(this.producto);
   }
   
+  onMouseMove(evento) {
+    const elementoContenedorZoom = this.elementoContenedorZoom.nativeElement;
+    const elementoImagenZoom = this.elementoImagenZoom.nativeElement;
+
+    let desplazamientoContenedorX = evento.clientX - elementoContenedorZoom.offsetLeft;
+    let porcentajeX = desplazamientoContenedorX/(elementoContenedorZoom.offsetWidth - 2);
+    let desplazamientoImagenX = `${-1*elementoImagenZoom.offsetWidth*porcentajeX}px`;
+
+    let desplazamientoContenedorY = evento.clientY - elementoContenedorZoom.offsetTop;
+    let porcentajeY = desplazamientoContenedorY/(elementoContenedorZoom.offsetHeight - 2);
+    let desplazamientoImagenY = `${-1*elementoImagenZoom.offsetHeight*porcentajeY}px`;
+
+    elementoImagenZoom.style.transform = `translate(${desplazamientoImagenX}, ${desplazamientoImagenY})`;
+  }
+
+  onMouseLeave(evento) {
+    this.elementoImagenZoom.nativeElement.style.transform = 'none';
+  }
 }

f901d0e59ecf5eac6507dc6968ca49f21985ed38
wolfdark87 <david.roma.878@gmail.com>
2021-10-24 23:05:23 -0500

    feat(moda) integracion con componente detalle productos.

--- src/app/views/moda/moda-detalle/moda-detalle.component.ts
+++ src/app/views/moda/moda-detalle/moda-detalle.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 
 import { ModaService } from '../moda.service';
 import { ProductoInterface } from '../../../core/interfaces/producto.interface';
-import { CarritoService } from '../../../core/services/carrito.service';
 
 @Component({
   selector: 'bodoquin-moda-detalle',
@@ -16,8 +15,7 @@ export class ModaDetalleComponent implements OnInit {
   
   constructor(
     private route: ActivatedRoute,
-    private modaService: ModaService,
-    private carritoService: CarritoService) {}
+    private modaService: ModaService) {}
     
   ngOnInit() {
     this.getInfoProducto();
@@ -26,27 +24,9 @@ export class ModaDetalleComponent implements OnInit {
   getInfoProducto() {
     // Capturar el id de la url => Ok
     const idProducto = this.route.snapshot.params.idProducto;
-    console.log(idProducto);
     
     // Obtener la información del producto de acuerdo a su id => Ok
-    this.producto = this.modaService.obtenerProducto(idProducto);
-    console.log(this.producto);
-    
-    // mostrar el producto en el html => Ok    
-    
-    // colocar el diseño de la vista de detalle => Ok    
-    
-    // integrarnos al componente de carrito => Ok
-    
-    // integrarnos al componente de tabs
-  }
-  
-  sumarProducto() {
-    this.carritoService.productoSumado.emit(this.producto);
-  }
-  
-  restarProducto() {
-    this.carritoService.productoRestado.emit(this.producto);
+    this.producto = this.tecnologiaService.obtenerProducto(idProducto);
   }
   
 }

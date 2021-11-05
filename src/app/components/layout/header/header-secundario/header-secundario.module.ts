211a17296132c7ab3a11add14c60e85b9fd189f0
wolfdark87 <david.roma.878@gmail.com>
2021-11-04 23:56:51 -0500

    feat(header-secundario) implementa routing.

--- src/app/components/layout/header/header-secundario/header-secundario.module.ts
+++ src/app/components/layout/header/header-secundario/header-secundario.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HeaderSecundarioComponent } from './header-secundario.component';
 
 @NgModule({
+  imports: [
+    RouterModule
+  ],
   declarations: [
     HeaderSecundarioComponent,    
   ],

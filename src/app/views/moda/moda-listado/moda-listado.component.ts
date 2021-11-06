b93ae3a5a770f6401d6fd8f556f49058fb1898c2
wolfdark87 <david.roma.878@gmail.com>
2021-11-06 00:27:58 -0500

    feat(mopda) implkementa metodos

--- src/app/views/moda/moda-listado/moda-listado.component.ts
+++ src/app/views/moda/moda-listado/moda-listado.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { ProductoInterface } from '../../../core/interfaces/producto.interface';
-import { CarritoService } from '../../../core/services/carrito.service';
 import { ModaService } from '../moda.service';
 
 @Component({
@@ -15,7 +14,6 @@ export class ModaListadoComponent implements OnInit {
   productos: Array<ProductoInterface> = [];
   
   constructor(
-    private carritoService: CarritoService,
     private modaService: ModaService,
     private router: Router) {}
     
@@ -25,26 +23,6 @@ export class ModaListadoComponent implements OnInit {
   
   obtenerProductos() {
     this.productos = this.modaService.obtenerProductos();
-  }
-    
-  onClickAgregar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoAgregado.emit(producto);
-  }
-  
-  onClickSumar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoSumado.emit(producto);
-  }
-  
-  onClickRestar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoRestado.emit(producto);
-  }
-  
-  onClickEliminar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoEliminado.emit(producto);
   }
   
   onClickTarjeta(producto: ProductoInterface) {

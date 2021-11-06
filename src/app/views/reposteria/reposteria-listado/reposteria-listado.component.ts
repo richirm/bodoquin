2f17832a961f733de47f65b13410509ac5824dc7
richirm <richirm89@hotmail.com>
2021-11-06 00:25:39 -0500

    fix(reposteria-listado.component): remueve código inutilizado

--- src/app/views/reposteria/reposteria-listado/reposteria-listado.component.ts
+++ src/app/views/reposteria/reposteria-listado/reposteria-listado.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { ProductoInterface } from '../../../core/interfaces/producto.interface';
-import { CarritoService } from '../../../core/services/carrito.service';
 import { ReposteriaService } from '../reposteria.service';
 
 @Component({
@@ -15,7 +14,6 @@ export class ReposteriaListadoComponent implements OnInit {
   productos: Array<ProductoInterface> = [];
   
   constructor(
-    private carritoService: CarritoService,
     private reposteriaService: ReposteriaService,
     private router: Router) {}
     
@@ -25,26 +23,6 @@ export class ReposteriaListadoComponent implements OnInit {
   
   obtenerProductos() {
     this.productos = this.reposteriaService.obtenerProductos();
-  }
-    
-  onClickAgregar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoAgregado.emit(producto);
-  }
-  
-  onClickSumar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoSumado.emit(producto);
-  }
-  
-  onClickRestar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoRestado.emit(producto);
-  }
-  
-  onClickEliminar(producto: ProductoInterface) {
-    event.stopPropagation();
-    this.carritoService.productoEliminado.emit(producto);
   }
   
   onClickTarjeta(producto: ProductoInterface) {

f71e6aff8b449d1185a1c38974ff9c983e60da6b
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.nttdata.com>
2021-12-29 23:28:05 -0500

    feat(jugueteria-listado.component): integración con el endpoint de musica

--- src/app/views/jugueteria/jugueteria-listado/jugueteria-listado.component.ts
+++ src/app/views/jugueteria/jugueteria-listado/jugueteria-listado.component.ts
@@ -5,6 +5,8 @@ import { ProductoInterface } from '../../../core/interfaces/producto.interface';
 import { JugueteriaService } from '../jugueteria.service';
 import { Jugueteria2Service } from '../jugueteria2.service';
 
+import { MusicaInterface } from '../interfaces/musica.interface';
+
 @Component({
   selector: 'bodoquin-jugueteria-listado',
   templateUrl: './jugueteria-listado.component.html',
@@ -13,16 +15,17 @@ import { Jugueteria2Service } from '../jugueteria2.service';
 export class JugueteriaListadoComponent implements OnInit {
   
   productos : Array<ProductoInterface> = [];
+  musicas: Array<MusicaInterface> = [];
   
   constructor(
     private jugueteriaService: JugueteriaService,
-	private jugueteria2Service: Jugueteria2Service,
+    private jugueteria2Service: Jugueteria2Service,
     private router: Router) {}
     
   ngOnInit() {
     this.obtenerProductos();    
 	
-	const musicas = this.jugueteria2Service.getMusicasCache();
+    const musicas = this.jugueteria2Service.getMusicasCache();
     console.log(musicas);
 
     const precio = this.jugueteria2Service.getMusicaPrecioLocal();
@@ -31,7 +34,15 @@ export class JugueteriaListadoComponent implements OnInit {
     const genero = this.jugueteria2Service.getMusicaGeneroSession();
     console.log(genero);
 
-    // this.jugueteria2Service.getAlumnosBackend();
+    this.jugueteria2Service.getMusicaBackend().subscribe(
+      (response) => {
+        console.log(response);
+        this.musicas = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
   
   obtenerProductos() {

e6b9c10d8d0a1dc20842503c040c03dd0e25bfaa
richirm <richirm89@hotmail.com>
2022-01-05 22:46:01 -0500

    feat(reposteria.service): metodo obtenerProductos retorna el tipo Observable

--- src/app/views/reposteria/reposteria.service.ts
+++ src/app/views/reposteria/reposteria.service.ts
@@ -13,7 +13,7 @@ export class ReposteriaService {
   constructor(
     private httpClient: HttpClient) {}
   
-  obtenerProductos() {
+  obtenerProductos(): Observable<Array<ProductoInterface>> {
     return this.httpClient.get<Array<ProductoInterface>>('http://localhost:3000/productos/reposteria');
   }
   

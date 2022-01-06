95f1a13edbe32ef0a5d21bbb8205c3c4098dca0c
wolfdark87 <david.roma.878@gmail.com>
2022-01-05 23:31:09 -0500

    feat(moda)  implementa get en vista de moda .

--- src/app/views/moda/moda.service.ts
+++ src/app/views/moda/moda.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { ProductoInterface } from '../../core/interfaces/producto.interface';
 import { ModaConstants } from './moda.constants';
@@ -8,14 +10,17 @@ export class ModaService {
   
   productos: Array<ProductoInterface> = ModaConstants.PRODUCTOS;
   
-  obtenerProductos(): Array<ProductoInterface> {
-    return this.productos;
+  constructor(
+    private httpClient: HttpClient
+  ) {}
+  
+  obtenerProductos() {
+    return this.httpClient.get<Array<ProductoInterface>>('http://localhost:3000/productos/moda');
   }
     
   obtenerProducto(idProducto: number): ProductoInterface {
     return this.productos.find((producto: ProductoInterface) => {
       return producto.idProducto === Number(idProducto);
     });
-  }
-  
+  } 
 }

eda2a50e466d061d1c5a955f2fcaf8f4ba0fe725
wolfdark87 <david.roma.878@gmail.com>
2022-01-05 23:53:34 -0500

    tecnologia

--- src/app/views/tecnologia/tecnologia.service.ts
+++ src/app/views/tecnologia/tecnologia.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { ProductoInterface } from '../../core/interfaces/producto.interface';
 import { TecnologiaConstants } from './tecnologia.constants';
@@ -10,10 +11,9 @@ export class TecnologiaService {
   productos: Array<ProductoInterface> = TecnologiaConstants.PRODUCTOS;
   
   constructor(
-    private httpClient: HttpClient
-  ) {}
+    private httpClient: HttpClient) {}
   
-  obtenerProductos() {
+  obtenerProductos(): Observable<Array<ProductoInterface>> {
     return this.httpClient.get<Array<ProductoInterface>>('http://localhost:3000/productos/tecnologia');
   }
     

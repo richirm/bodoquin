f725f061c241e56c7c4001bbf9caa56076959e42
Vanessa Chipana <vanessa.cintya.chipana.isidro@everis.nttdata.com>
2022-01-06 00:01:56 -0500

    feat(jugueteria-listado): Integración con endpoint GET /jugueteria

--- src/app/views/jugueteria/jugueteria.service.ts
+++ src/app/views/jugueteria/jugueteria.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 import { ProductoInterface } from '../../core/interfaces/producto.interface';
 import { JugueteriaConstants } from './jugueteria.constants';
@@ -8,8 +10,11 @@ export class JugueteriaService {
   
   productos: Array<ProductoInterface> = JugueteriaConstants.PRODUCTOS;
   
-  obtenerProductos(): Array<ProductoInterface> {
-    return this.productos;
+  constructor(
+    private httpClient: HttpClient) {}
+  
+  obtenerProductos(): Observable<Array<ProductoInterface>> {
+    return this.httpClient.get<Array<ProductoInterface>>('http://localhost:3000/productos/jugueteria');
   }
 
   obtenerProducto(idProducto: number): ProductoInterface {

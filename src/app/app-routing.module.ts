52c1c8777d6c2da7184232a67efb8f1efddf25c7
wolfdark87 <david.roma.878@gmail.com>
2022-09-06 21:50:05 -0500

    feat(tecnologia) modificacion de module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './core/guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -17,7 +19,9 @@ const routes: Routes = [
   },
   {
     path: 'tecnologia',
-    loadChildren: () => import('./views/tecnologia/tecnologia.module').then(m => m.TecnologiaModule)
+    loadChildren: () => import('./views/tecnologia/tecnologia.module').then(m => m.TecnologiaModule),
+    canActivate: [AuthGuard],
+    canDeactivate: [AuthGuard],
   },
   {
     path: 'jugueteria',

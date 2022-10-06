ba00186732dd401166a1048c9dac52e8ddaa5ffc
wolfdark87 <david.roma.878@gmail.com>
2022-10-06 00:04:59 -0500

    feat(Videojuegos): creacion de la estructura de componentes.

---
+++ src/app/views/videojuegos/videojuegos-listado/videojuegos-listado.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bodoquin-videojuegos-listado',
+  templateUrl: './videojuegos-listado.component.html',
+  styleUrls: ['./videojuegos-listado.component.scss']
+})
+export class VideojuegosListadoComponent {
+  
+}
---
+++ src/app/views/videojuegos/videojuegos.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { VideojuegosComponent } from './Videojuegos.component';
+import { VideojuegosListadoComponent } from './videojuegos-listado/videojuegos-listado.component';
+import { VideojuegosDetalleComponent } from './videojuegos-detalle/videojuegos-detalle.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  
+  declarations: [
+    VideojuegosComponent,
+    VideojuegosListadoComponent,
+    VideojuegosDetalleComponent
+  ],
+  
+  exports: [
+    VideojuegosComponent,
+    VideojuegosListadoComponent,
+    VideojuegosDetalleComponent
+  ],
+})
+export class VideojuegosModule {}

f0602dbd048560093cefbca59742b5f3249eab5a
wolfdark87 <david.roma.878@gmail.com>
2022-10-08 23:21:14 -0500

    feat(videojuegos): logica para mostrar las tarjeta de productos videojuegos e integracion con el endpoint de videojuegos.

--- src/app/views/videojuegos/videojuegos.module.ts
+++ src/app/views/videojuegos/videojuegos.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { TarjetasProductoModule } from '../../components/tarjetas-producto/tarjetas-producto.module';
+
 import { VideojuegosComponent } from './Videojuegos.component';
 import { VideojuegosListadoComponent } from './videojuegos-listado/videojuegos-listado.component';
 import { VideojuegosDetalleComponent } from './videojuegos-detalle/videojuegos-detalle.component';
 
 import { VideojuegosRoutingModule } from './videojuegos-routing.module';
+import { VideojuegosService } from './videojuegos.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    
+    TarjetasProductoModule,
     
     VideojuegosRoutingModule,
   ], 
@@ -25,5 +30,8 @@ import { VideojuegosRoutingModule } from './videojuegos-routing.module';
     VideojuegosListadoComponent,
     VideojuegosDetalleComponent
   ],
+  providers: [
+    VideojuegosService,
+  ]
 })
 export class VideojuegosModule {}
---
+++ src/app/views/videojuegos/videojuegos.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { ProductoInterface } from '../../core/interfaces/producto.interface';
+
+@Injectable()
+export class VideojuegosService {
+    
+  constructor(
+    private httpClient: HttpClient) {}
+  
+  obtenerProductos(): Observable<Array<ProductoInterface>> {
+    return this.httpClient.get<Array<ProductoInterface>>('http://localhost:3000/productos/videojuegos');
+  }
+}

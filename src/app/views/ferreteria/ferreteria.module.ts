7c2d739abd5c85e93725d6f7e2df03312f02e092
wolfdark87 <david.roma.878@gmail.com>
2022-10-17 23:34:03 -0500

    feat(ferreteria) creacion de pantalla de listado .

--- src/app/views/ferreteria/ferreteria.module.ts
+++ src/app/views/ferreteria/ferreteria.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TarjetasProductoModule } from '../../components/tarjetas-producto/tarjetas-producto.module';
+
 import { FerreteriaComponent } from './ferreteria.component';
 import { FerreteriaListadoComponent } from './ferreteria-listado/ferreteria-listado.component';
 import { FerreteriaDetalleComponent } from './ferreteria-detalle/ferreteria-detalle.component';
 import { FerreteriaRoutingModule } from './ferreteria-routing.module';
+
+import { FerreteriaService } from './ferreteria.service';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { FerreteriaRoutingModule } from './ferreteria-routing.module';
   imports: [
     CommonModule,
     FerreteriaRoutingModule,
+    TarjetasProductoModule,
+  ],
+  providers: [
+    FerreteriaService,
   ]
 })
 export class FerreteriaModule { }

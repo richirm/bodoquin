0e932ce7f426a2ae1d414b0b40a3580f4a26beb1
wolfdark87 <david.roma.878@gmail.com>
2022-11-04 22:58:57 -0500

    feat(productos-naturales) creacion de ruta

--- src/app/views/productos-naturales/productos-naturales-routing.module.ts
+++ src/app/views/productos-naturales/productos-naturales-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ProductosNaturalesComponent } from './productos-naturales.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductosNaturalesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
